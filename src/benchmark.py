9f9a9cb0cffbd81cd756de3b13b62d103e618630
humphrja <103923110+humphrja@users.noreply.github.com>
2025-08-09 09:14:48 +1000

    Modify benchmark script to accept single test case and first guess params

--- src/benchmark.py
+++ src/benchmark.py
@@ -20,81 +20,100 @@ from time import time
 
 from argparse import ArgumentParser
 
+MAX_GUESSES = 6
+FAIL_SCORE = 7
+
+def output_result(solved: bool, score: int, soln: str, game_num: int, total_games: int):    
+    if solved:
+        print(f"[{game_num}/{total_games}] Bot solved '{soln}' in {score} guesses.")
+    else:
+        print(f"[{game_num}/{total_games}] Bot failed to solve '{soln}' within {MAX_GUESSES} guesses.")
+
+
+def game_loop(app: WordleApp, soln: str, first_guess: str) -> tuple[bool, int]:
+    """ Returns if the bot solved wordle, and the corresponding game score. """
+    # Create a new bot for this game
+    bot = WordleSolver()
+
+    # Set the answer for the app
+    try:
+        app.set_answer(soln)
+    except ValueError as e:
+        # raise ValueError(e)
+        print(f"Unexpected solution '{soln}': {e}")
+        # Do not validate against expected word list
+        app.set_answer(soln, validate=False)
+        bot.wordle_list.possible_solutions = bot.wordle_list.all_guesses.copy()
+    
+    # Start the game loop
+    guesses = []
+    
+    for n in range(MAX_GUESSES):
+        if n == 0:
+            guess = first_guess
+        else:
+            # Bot makes a guess
+            guess = bot.make_guess()
+
+        # App provides feedback
+        solved, feedback = app.check_guess(guess)
+
+        # Display result (Y/M/N) for yes/maybe/no
+        guess_str = app.make_guess_str(guess, feedback)
+        guesses.append(guess_str)
+        for g in guesses:
+            print(g)
+
+        if solved:
+            return True, n+1
+
+        # Apply feedback to the bot
+        bot.apply_feedback(guess, feedback)
+
+    return False, FAIL_SCORE
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Wordle Bot Benchmarking Script")
     parser.add_argument('--version', '-v', type=str, help="Version of the bot being tested", required=True)
+    parser.add_argument('--test_case', '-t', type=str, help="Single solution word to test against (optional)", required=False, default=None)
+    parser.add_argument('--first_guess', '-fg', type=str, help="First guess bot should maket (optional)", required=False, default=None)
     args = parser.parse_args()
-
-    # Load live-responses data
-    game_history = pd.read_csv("data/benchmark_results.csv")
-    game_history = game_history.iloc[:] # Truncate if needed
-
-    max_guesses = 6
-    fail_score = 7
 
     app = WordleApp()
 
     bot_scores = []
-    bot = WordleSolver()
-
     start_time = time()
 
-    print(f"Calculating first guess...")
-    first_guess = bot.make_guess()
-    print(f"First guess: {first_guess} ({time() - start_time:.2f} s)")
+    if args.first_guess:
+        first_guess = args.first_guess.upper()
+        print(f"Using provided first guess: {first_guess}")
+    else:
+        print(f"Calculating first guess...")
+        bot = WordleSolver()
+        first_guess = bot.make_guess()
+        print(f"First guess: {first_guess} ({time() - start_time:.2f} s)")
+
+    if args.test_case:
+        solved, score = game_loop(app, args.test_case, first_guess)
+        output_result(solved, score, args.test_case, 1, 1)
+        exit()
+
+    # Load the live responses data
+    game_history = pd.read_csv("data/benchmark_results.csv")
+    game_history = game_history.iloc[:3] # Truncate if needed
 
     for i, soln in enumerate(game_history['solution']):
-        # Create a new bot for this game
-        bot = WordleSolver()
+        solved, score = game_loop(app, soln, first_guess)
 
-        # Set the answer for the app
-        try:
-            app.set_answer(soln)
-        except ValueError as e:
-            # raise ValueError(e)
-            print(f"Unexpected solution '{soln}': {e}")
-            # Do not validate against expected word list
-            app.set_answer(soln, validate=False)
-            bot.wordle_list.possible_solutions = bot.wordle_list.all_guesses.copy()
-        
-        # Start the game loop
-        guesses = []
-        
-        for _ in range(max_guesses):
-            if len(guesses) == 0:
-                guess = first_guess
-            else:
-                # Bot makes a guess
-                guess = bot.make_guess()
-
-            # App provides feedback
-            solved, feedback = app.check_guess(guess)
-
-            # Display result (Y/M/N) for yes/maybe/no
-            guess_str = app.make_guess_str(guess, feedback)
-            guesses.append(guess_str)
-            for g in guesses:
-                print(g)
-
-            if solved:
-                break
-
-            # Apply feedback to the bot
-            bot.apply_feedback(guess, feedback)
-
-        if solved:
-            score = len(guesses)
-            print(f"[{i+1}/{len(game_history)}] Bot solved '{soln}' in {score} guesses.")
-        else:
-            score = fail_score   # Strongly penalise failed solves
-            print(f"[{i+1}/{len(game_history)}] Bot failed to solve '{soln}' within {max_guesses} guesses.")
+        output_result(solved, score, soln, i+1, len(game_history))
 
         bot_scores.append(score)
 
     elapsed_time = time() - start_time
     print(f"Total duration: {elapsed_time:.2f} s")
 
-    # Save the results to a CSV file
-    game_history[f'bot_v{args.version}'] = bot_scores
+    if args.test_case is None:
+        # Save the results to a CSV file
+        game_history[f'bot_v{args.version}'] = bot_scores
 
-    game_history.to_csv(f"data/benchmark_results_v{args.version}.csv", index=False)
+        game_history.to_csv(f"data/benchmark_results_v{args.version}.csv", index=False)
